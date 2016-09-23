146282595c5e135df6773a5fdf478b6281e79b40
galbini <galbini@genoscope.cns.fr>
2016-09-23 11:57:00 +0200

    change attrs by attr in function parameter to avoid undefined error

--- src/directives/udt-change.js
+++ src/directives/udt-change.js
@@ -2,7 +2,7 @@ angular.module('ultimateDataTableServices').
 directive('udtChange', ['$interval', function($interval) {
 	return {
 		require: 'ngModel',
-		link: function(scope, element, attrs, ngModel) {
+		link: function(scope, element, attr, ngModel) {
 			scope.$watch(attr.ngModel, function(newValue, oldValue){
 				if(newValue !== oldValue){
 					scope.$eval(attr.udtChange);
