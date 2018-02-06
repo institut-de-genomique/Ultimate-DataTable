89ab3d40ffdf0e06e8a7e6d0aa76521df6a0aa90
Guillaume Albini <galbini@genoscope.cns.fr>
2018-02-06 14:47:36 +0100

    improve directive dateTimestamp

--- src/directives/udt-dateTimestamp.js
+++ src/directives/udt-dateTimestamp.js
@@ -1,50 +1,28 @@
 angular.module('ultimateDataTableServices').
  //Convert the date in format(view) to a timestamp date(model)
-directive('udtDateTimestamp', function() {
-	            return {
-	                require: 'ngModel',
-	                link: function(scope, ele, attr, ngModel) {
-						var typedDate = "01/01/1970";//Initialisation of the date
-						
-	                	var convertToDate = function(date){
-	                		if(date !== null && date !== undefined && date !== ""){
-		                		var format = scope.udtTableFunctions.messages.Messages("date.format").toUpperCase();
-		                		date = moment(date).format(format);
-		                		return date;
-	                		}
-	                		return "";
-	                	};
-	                	
-	                	var convertToTimestamp = function(date){
-	                		if(date !== null && date !== undefined && date !== ""){
-		                		var format = scope.udtTableFunctions.messages.Messages("date.format").toUpperCase();
-		    					return moment(date, format).valueOf();
-	                		}
-	                		return "";
-	    				};
-						
-	                	//model to view
-	                	scope.$watch(
-							function(){
-								return ngModel.$modelValue;
-							}, function(newValue, oldValue){
-								//We check if the
-								if(newValue !== null && newValue !== undefined && newValue !== "" && typedDate.length === 10){
-									var date = convertToDate(newValue);
-	    							ngModel.$setViewValue(date);
-									ngModel.$render();
-								}
-	                    });
-						
-	                	//view to model
-	                    ngModel.$parsers.push(function(value) {
-	                    	var date = value;
-							typedDate = date;//The date of the user
-	                    	if(value.length === 10){//When the date is complete
-	                    		date = convertToTimestamp(value);
-	                    	}
-							return date;
-	                    });
-	                }
-	            }
-	        });
+directive('udtDateTimestamp', ['$filter', function($filter) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModelController) {
+			
+			ngModelController.$formatters.push(function(data) {
+				var convertedData = data;
+				convertedData = $filter('date')(convertedData, Messages("date.format"));
+			    return convertedData;
+			}); 
+	    
+		    ngModelController.$parsers.push(function(data) {
+		    	var convertedData = data;
+	    	    if(moment && convertedData !== ""){
+	    			   convertedData = moment(data, Messages("date.format").toUpperCase()).valueOf();
+	    		   }else{
+	    			   convertedData = null;
+	    			   console.log("mission moment library to convert string to date");
+	    		   }
+		    	   
+		    	  return convertedData;
+		    }); 
+			
+        }
+    }
+}]);
