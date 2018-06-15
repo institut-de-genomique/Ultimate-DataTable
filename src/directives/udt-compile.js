fac578ca737faeddcbeaf0ad36b4e80bcf2df120
Guillaume Albini <galbini@genoscope.cns.fr>
2018-06-15 10:49:00 +0200

    remove watch on udtCompile attribut. improve performance

--- src/directives/udt-compile.js
+++ src/directives/udt-compile.js
@@ -4,23 +4,34 @@ directive('udtCompile', ['$compile', function($compile) {
 			return {
 				restrict: 'A',
   		    	link: function(scope, element, attrs) {
-  				    scope.$watch(
+  				    /*
+  		    		 * Normaly no need to watch because the attribute udtCompile not change during the life of UDT
+  		    		scope.$watch(
   				        function(scope) {
   				             // watch the 'compile' expression for changes
   				            return scope.$eval(attrs.udtCompile);
   				        },
-  				        function(value) {
-  				            // when the 'compile' expression changes
-  				            // assign it into the current DOM
-  				            element.html(value);
+  				        function(newValue, oldValue) {
+  				        	//if(newValue !== oldValue){
+  				        		console.log("watch udtCompile");
+  	  				            // when the 'compile' expression changes
+  	  				            // assign it into the current DOM
+  	  				            element.html(newValue);
 
-  				            // compile the new DOM and link it to the current
-  				            // scope.
-  				            // NOTE: we only compile .childNodes so that
-  				            // we don't get into infinite loop compiling ourselves
-  				            $compile(element.contents())(scope);
+  	  				            // compile the new DOM and link it to the current
+  	  				            // scope.
+  	  				            // NOTE: we only compile .childNodes so that
+  	  				            // we don't get into infinite loop compiling ourselves
+  	  				            $compile(element.contents())(scope);
+  				        	//}
+  				        	
   				        }
   				    );
+  		    		*/
+  		    		var value = scope.$eval(attrs.udtCompile);
+  		    		element.html(value);
+  		    		$compile(element.contents())(scope);
+  		    		
   				}
 			};
 						
