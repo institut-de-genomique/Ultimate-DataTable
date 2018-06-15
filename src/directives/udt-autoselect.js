a64ae36320c95b947b12b283ca32290f8912acb4
Guillaume Albini <galbini@genoscope.cns.fr>
2018-06-15 10:46:40 +0200

    manage array on autoSelect

--- src/directives/udt-autoselect.js
+++ src/directives/udt-autoselect.js
@@ -1,25 +1,34 @@
 angular.module('ultimateDataTableServices').
 //If the select or multiple choices contain 1 element, this directive select it automaticaly
 //EXAMPLE: <select ng-model="x" ng-option="x as x for x in x" udtAutoselect>...</select>
-directive('udtAutoselect',['$parse', function($parse) {
+directive('udtAutoSelect',['$parse', function($parse) {
     		var OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w\d]*)|(?:\(\s*([\$\w][\$\w\d]*)\s*,\s*([\$\w][\$\w\d]*)\s*\)))\s+in\s+(.*)$/;
     		return {
     			require: 'ngModel',
     			link: function(scope, element, attrs, ngModel) {
     				var valOption = undefined;
+					var multiple = false;
 					if(attrs.ngOptions){	
 						valOption = attrs.ngOptions;
 					}else if(attrs.btOptions){
 						valOption = attrs.btOptions;
 					}
 					
-					if(valOption !== undefined){
+					if(attrs.multiple === true || attrs.multiple === "true"){
+						multiple = true;
+					}
+					
+					if(valOption != undefined){
 						var match = valOption.match(OPTIONS_REGEXP);
+						var getModelValue = $parse(match[1].replace(match[4]+'.',''));
 						var model = $parse(match[7]);
 						scope.$watch(model, function(value){
 							if(value){
-				                if(value.length === 1 && (ngModel.$modelValue === undefined || ngModel.$modelValue === "")){
-									ngModel.$setViewValue(value[0].code);
+								if(value.length === 1 && ngModel.$isEmpty(ngModel.$modelValue)){
+									
+									var value = (multiple)?[getModelValue(value[0])]:getModelValue(value[0]);
+									
+									ngModel.$setViewValue(value);
 									ngModel.$render();
 								}
 							}
