8e2d712bc81677934d41c06a6851f06ad9e0a91d
Guillaume Albini <galbini@genoscope.cns.fr>
2018-06-15 11:18:50 +0200

    rename ngModel to ngModelController

--- src/directives/udt-change.js
+++ src/directives/udt-change.js
@@ -2,7 +2,7 @@ angular.module('ultimateDataTableServices').
 directive('udtChange', function() {
 	return {
 	  require: 'ngModel',
-		link: function(scope, element, attr, ngModel) {
+		link: function(scope, element, attr, ngModelController) {
 		   scope.$watch(attr.ngModel, function(newValue, oldValue){
 				if(newValue !== oldValue){
 					scope.$evalAsync(attr.udtChange);						
